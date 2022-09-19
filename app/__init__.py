c1629916ceb04d98ba66a2b1ac6a2ccfbf372d00
Jose Carlos Pazos <jcpazos93@gmail.com>
2022-09-19 16:32:05 -0500

    add migrate config

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from config import Config
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #inicializar la aplicacion app con la configuracion Config y la base de datos db
 app = Flask(__name__)
 cors = CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
+#configuracion para migraciones
+migrate = Migrate(app, db)
 #importar las rutas y modelos por si queremos usarlos
 from app import models, topRoutes
 
